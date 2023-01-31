8c403711dc28930208fbf72c91f031230a232d46
zaynlannister <zaynlannister@gmail.com>
2023-01-31 19:46:43 +0500

    feat: add toggle like func

--- src/posts.js
+++ src/posts.js
@@ -1,7 +1,7 @@
 export const responseFromServer = [
     {
         username: "jonny123",
-        id: 1,
+        id: 0,
         src: "https://img2.akspic.ru/previews/4/8/5/9/6/169584/169584-malenkie_koshmary_2-malo_koshmary-dolgopyatnye_studii-mono_malenkie_koshmary-igra_uzhasov-x750.jpg",
         likes: 124,
         description: "замечатльное фото..."
@@ -9,7 +9,7 @@ export const responseFromServer = [
 
     {
         username: 'Lara_queen',
-        id: 2,
+        id: 1,
         src: "https://img2.akspic.ru/previews/6/7/5/1/7/171576/171576-oblako-rastenie-gora-dnevnoe_vremya-ekoregion-500x.jpg",
         likes: 12,
         description: "ууу"
@@ -17,7 +17,7 @@ export const responseFromServer = [
 
     {
         username: 'oleg1mazelov',
-        id: 3,
+        id: 2,
         src: "https://img2.akspic.ru/previews/1/7/5/1/7/171571/171571-rastitelnost-rastenie-gora-zelenyj-oblako-x750.jpg",
         likes: 566,
         description: "spectacular view!"
