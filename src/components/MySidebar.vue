ef2afe8bee7bdc94ca07a326e2c00edf59272c3c
zaynlannister <zaynlannister@gmail.com>
2023-02-17 21:30:26 +0500

    add toggleNav func

--- src/components/MySidebar.vue
+++ src/components/MySidebar.vue
@@ -4,22 +4,52 @@
       <img src="@/assets/instagram_logo.png" alt="logo">
     </div>
     <div class="sidebar__nav">
-      <div><a href="#">Главная</a></div>
-      <div><a href="#">Поисковый запрос</a></div>
-      <div><a href="#">Интересные</a></div>
-      <div><a href="#">Reels</a></div>
-      <div><a href="#">Сообщения</a></div>
-      <div><a href="#">Уведомления</a></div>
-      <div><a href="#">Создать</a></div>
-      <div><a href="#">Профиль</a></div>
-      <div><a href="#">Ещё</a></div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <svg aria-label="Главная" class="_ab6-" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z"></path></svg>
+        <a data-id="nav-link" href="#">Главная</a>
+      </div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <a data-id="nav-link" href="#">Поисковый запрос</a>
+      </div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <a data-id="nav-link" href="#">Интересные</a>
+      </div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <a data-id="nav-link" href="#">Reels</a>
+      </div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <a data-id="nav-link" href="#">Сообщения</a>
+      </div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <a data-id="nav-link" href="#">Уведомления</a>
+      </div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <a data-id="nav-link" href="#">Создать</a>
+      </div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <a data-id="nav-link" href="#">Профиль</a>
+      </div>
+      <div @click="toggleNav($event.target)" data-id="nav-link" class="sidebar__nav-link">
+        <a data-id="nav-link" href="#">Ещё</a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    toggleNav(clickedItem) {
+      console.log(clickedItem)
+      if (clickedItem.getAttribute("nav-link")) {
+        const navLinks = document.querySelectorAll(".sidebar__nav-link");
 
+        navLinks.forEach(item => {
+          console.log('sd')
+        })
+      }
+    }
+  }
 }
 </script>
 
@@ -63,6 +93,15 @@ export default {
         text-decoration: none;
         color: #1f1f1f;
       }
+    }
+  }
+
+  .sidebar__nav-link {
+    display: flex;
+    align-items: center;
+
+    &.active {
+      font-weight: bold;
     }
   }
 </style>
